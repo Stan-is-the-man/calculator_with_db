ae32b5643d03c52eca621bd6ad141571aef58e56
Stanislav Yanakiev <stan5050@gmail.com>
2023-04-22 11:44:48 +0300

    'fixed factorial func'

--- main.py
+++ main.py
@@ -1,33 +1,39 @@
 def calculator(first_number, second_number, action):
     if action == "*":
-        return first_number * second_number
-    if action == "/":
-        return first_number / second_number
-    if action == "+":
-        return first_number + second_number
-    if action == "-":
-        return first_number - second_number
-    if action == "^":
-        return first_number ^ second_number
-    if action == "%":
-        return first_number % second_number
-    return 'Operation not allowed'
+        print(first_number * second_number)
+    elif action == "/":
+        print(first_number / second_number)
+    elif action == "+":
+        print((first_number + second_number))
+    elif action == "-":
+        print(first_number - second_number)
+    elif action == "^":
+        print(first_number ^ second_number)
+    elif action == "%":
+        print(first_number % second_number)
 
 
 def factorial(number):
     result = 1
     for num in range(1, number + 1):
-        result *= number
-    return result
+        result *= num
+    print(result)
 
 
 while True:
     operator = input("Please enter one of the following actions to perform:\n"
-                     " +, -, *, /, ^, %, ! ")
-    if operator != "!":
-        number_1 = float(input("Please enter the first number"))
-        number_2 = float(input("Please enter the second number"))
+                     "( +, -, *, /, ^, %, ! ) or type 'exit' to quit. \n")
+
+    if operator == 'exit':
+        break
+
+    elif operator in ['+', '-', '*', '/', '%']:
+        number_1 = float(input("Please enter the first number\n"))
+        number_2 = float(input("Please enter the second number\n"))
         calculator(number_1, number_2, operator)
-    else:
-        the_number = int(input("Please enter the first number"))
+
+    elif operator == "!":
+        the_number = int(input("Please enter just one number\n"))
         factorial(the_number)
+    else:
+        print('Operation not allowed:')
--- test.py
+++ test.py
@@ -2,9 +2,16 @@ def factorial(number):
     result = 1
 
     for num in range(1, number + 1):
-        result *= number
+        result *= num
     return result
 
 
 print(factorial(5))
+
+
+# print(25%100)
+
+
+
+
 
